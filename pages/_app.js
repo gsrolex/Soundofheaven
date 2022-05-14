0979cb7315509b8468f63f86d1b5d4ea3b313bef
gsrolex <31456849+gsrolex@users.noreply.github.com>
2022-05-14 21:26:13 +0200

    script bootstrap

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 /* import "bootstrap/dist/css/bootstrap.css"; */
 import "../styles/globals.css";
 import "../css/style.css";
+import Script from "next/script";
 
 export default function MyApp({ Component, pageProps }) {
   return (
@@ -9,11 +10,11 @@ export default function MyApp({ Component, pageProps }) {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <script
+      <Script
         src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
         crossorigin="anonymous"
-      ></script>
+      ></Script>
       <Component {...pageProps} />
     </>
   );
